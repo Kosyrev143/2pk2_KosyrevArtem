a970e95e0bb8df656cf7ac7849eb9365c8e7fdf4
kosyrevartem76@gmail.com <kosyrevartem76@gmail.com>
2023-09-15 16:18:06 +0500

    добавлена 2 практика

---
+++ pz_02/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.Write("x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            double s, t, v;
+
+            if (y < 2)
+            {
+                s = (y - y * Math.Pow(x, 2)) / (x + 1);
+            }
+            else
+            {
+                s = -10.6 * x * y;
+            }
+
+            if (s <= 0)
+            {
+                t = y * s + Math.Sin(x) + y;
+            }
+            else
+            {
+                t = s - Math.Pow(Math.Cos(s / x), 2);
+            }
+
+            v = 2 * y * x + 3 * y * s - s * t;
+
+            Console.WriteLine($"s: {Math.Round(s, 2)}");
+            Console.WriteLine($"t: {Math.Round(t, 2)}");
+            Console.WriteLine($"v: {Math.Round(v, 2)}");
+        }
+    }
+}
