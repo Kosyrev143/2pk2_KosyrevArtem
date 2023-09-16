61272605bd294c1e0c0e0b9b3ce9bfdf5b82dc68
kosyrevartem76@gmail.com <kosyrevartem76@gmail.com>
2023-09-16 08:44:31 +0500

    добавлена 3 практика

---
+++ pz_03/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Отгадайте загадку");
+                Console.Write("Вот иголки и булавки\nВыползают из - под лавки,\nНа меня они глядят,\nМолока они хотят.");
+                Console.Write("Ответ: ");
+                // создаем переменную для ответа
+                string answerToTheRiddle = Console.ReadLine();
+
+
+                // проверка правльного ответа
+                switch (answerToTheRiddle.ToLower())
+                {
+                    case "еж":
+                        Console.WriteLine("Вы отгадали загадку!");
+                        break;
+
+                    case "сдаюсь":
+                        Console.WriteLine("Правильный ответ: еж");
+                        break;
+                    default:
+                        Console.WriteLine("Ответ неверный");
+                        break;
+                }
+                continue;
+            }
+        }
+    }
+}
