0315be3e2552d6ef468dc2fe1ef2cdb42cfd0744
kosyrevartem76@gmail.com <kosyrevartem76@gmail.com>
2023-09-22 14:59:25 +0500

    добавлена 5 практика

---
+++ pz_05/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число для которого нужно найти показтель степени числа двойки: ");
+            int N = int.Parse(Console.ReadLine());
+
+            // показатель степени
+            int degreeIndicator = 1;
+            do
+            {
+                degreeIndicator++;
+            } while (Math.Pow(2, degreeIndicator) != N);
+            Console.WriteLine(degreeIndicator);
+
+        }
+    }
+}
